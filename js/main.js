8c6739820daa3525634865946a6e42501a662bb6
lucyworship <lucyworship@qq.com>
2018-02-28 10:29:43 +0800

    02281029

--- js/main.js
+++ js/main.js
@@ -77,9 +77,9 @@ $(function () {
         {src:loadingPath+'y-1.png'},
         {src:loadingPath+'y-2.png'},
         {src:loadingPath+'y-3.png'},
-        {src:loadingPath+'y-6.png'},
-        {src:loadingPath+'drum-1.mp3'},
-        {src:loadingPath+'drum-2.mp3'}
+        {src:loadingPath+'y-6.png'}
+        // {src:loadingPath+'drum-1.mp3'},
+        // {src:loadingPath+'drum-2.mp3'}
 
     ];
 
